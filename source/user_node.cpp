3454c97fd988d2b6fccb3729470e8ec8bd6ac31b
Joel Savitz <theyoyojo@gmail.com>
2018-08-08 11:25:34 -0400

    A smidge more refactoring/documenting

--- source/user_node.cpp
+++ source/user_node.cpp
@@ -195,32 +195,33 @@ void UserNode::onDatagramRecipt(MicroBitEvent e) {
 
   _recvPacketBuffer = _uBit.radio.datagram.recv() ;
 
-  _uBit.serial.printf("DATAGRAM RECEIVED! (%d)\r\n",
-  _recvPacketBuffer[0]) ;
+  _uBit.serial.printf("Datagram Received! Raw contents: (%d). Interpreted: ",
+    _recvPacketBuffer[0]) ;
 
   // Unlike the other event handlers, the actions of this function
   // are dependant on the signal received first and the state second
   switch(_recvPacketBuffer[0]) {
     case SIG_R:
-      _uBit.serial.printf("GOT SIG_R\r\n") ;
+      _uBit.serial.printf("SIG_R. Reset the device to boot state.\r\n") ;
       // On recipt of the reset signal, the device should reset to the boot state
       _state = _bootState;
       break ;
     case SIG_A:
-      _uBit.serial.printf("GOT SIG_A\r\n") ;
+      _uBit.serial.printf("SIG_A. Conversion signal from Team A.\r\n") ;
       // State transition from LISTEN_A to TEAM_A on event SIG_A received
       if (_state == LISTEN_A) {
         _state = TEAM_A ;
       }
       break ;
     case SIG_B:
-      _uBit.serial.printf("GOT SIG_B\r\n") ;
+      _uBit.serial.printf("SIG_B. Conversion signal from Team B.\r\n") ;
       // State transition from LISTEN_B to TEAM_B on event SIG_B received
       if (_state == LISTEN_B) {
         _state = TEAM_B ;
       }
       break ;
     default:
+      _uBit.serial.printf("Unknown signal...\r\n") ;
       break ;
   }
 }
@@ -243,41 +244,48 @@ void UserNode::broadcastAnimation() {
 
 void UserNode::updateLoadingAnimation() {
 
-  /*
-  Instead of printing the entire animation at once, each call to this function
-  prints the next stage of the animation in order to return to the caller
-  as soon as possible and act on new user input
-  */
-
-  if (++_iTickLoadingAnimation >= ECG::Images::nFramesLoadingAnimation * _ticksPerFrameLoadingAnimation ) {
+  // Increment the position of the device in the animation,
+  // but it you have gone to far, go back to the beginning
+  if (++_iTickLoadingAnimation >= 
+  ECG::Images::nFramesLoadingAnimation * _ticksPerFrameLoadingAnimation ) {
     _iTickLoadingAnimation = 0 ;
   }
 
+  // Convert the current position in ticks to the current position in frame number and then print the frame
   _uBit.display.print(ECG::Images::loadingAnimation[_iTickLoadingAnimation / _ticksPerFrameLoadingAnimation]) ;
 }
 
 void UserNode::loop() {
+
+  // This switch statement controls what is written to the display every tick
   switch(_state) {
+
     case UNASSIGNED:
+      // Update the display with the correct frame of the loading animation
       updateLoadingAnimation() ;
       break ;
+
+    // Lowecase letters represent a listening state
     case LISTEN_A:
       _uBit.display.print("a") ;
       break ;
     case LISTEN_B:
       _uBit.display.print("b") ;
       break ;
+
+    // Capital letters represent a team state
     case TEAM_A:
       _uBit.display.print("A") ;
       break ;
     case TEAM_B:
       _uBit.display.print("B") ;
       break ;
+
+    // This case is here for style purposes and is not used
     default:
       break ;
   }
 
-  // TODO: printf state every n ticks?
-
+  // After each iteration of this loop ("tick"), wait until it is time for the next one
   _uBit.sleep(_msDeviceTick) ;
 }
